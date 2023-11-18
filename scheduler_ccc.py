357cb1dc74d99b3c65d4d3eb9939d20b9dd2bdbc
apaarb2 <149335852+apaarb2@users.noreply.github.com>
2023-11-18 16:17:33 -0500

    Implement Hybrid Container Selection in SchedulerCCC
    Feature Addition:
    Implemented a new container selection strategy in the SchedulerCCC class, combining elements from the Hermod and Cypress approaches.
    Added a container_load dictionary to track the load of each container, enhancing the decision-making process for workload allocation.
    
    Changes in process method:
    Introduced logic to handle cases where no containers are available by requesting a new container.
    Incorporated a new select_container method to determine the most suitable container for the given workload.
    Added a fallback mechanism where a new container is requested if the initially selected container is unable to process the workload.
    
    New select_container method:
    First, the method tries to find a container that has already processed the given job type, prioritizing locality (inspired by Hermod's approach).
    
    If no such container is found, the method then selects a container based on the least load, taking into consideration the input size of the workload (aligning with Cypress's strategy).
    Reset Mechanism:
    
    Enhanced reset_implementation method to clear the container_load dictionary for new experiments, ensuring clean state management.
    
    General Note:
    This implementation aims to optimize workload distribution across containers, balancing between job familiarity (locality) and current load, with an eye towards efficiency and minimal latency.

--- scheduler_ccc.py
+++ scheduler_ccc.py
@@ -1,15 +1,43 @@
 from scheduler_interface import SchedulerInterface
+from random import choice
 
-# Cloud Computing Capstone Scheduler
 class SchedulerCCC(SchedulerInterface):
     def __init__(self, containers):
         super().__init__(containers)
-        pass
+        self.container_load = {}  # To keep track of container load
 
     def reset_implementation(self):
-        pass
+        # Reset the state for a new experiment
+        self.container_load = {}
 
     def process(self, workload):
-        # NOTE: call self.request_new_container() if there are
-        #       no available containers. 
-        pass
+        # If no containers are available, request a new one
+        if not self.containers:
+            self.request_new_container()
+
+        # Container selection strategy
+        selected_container = self.select_container(workload)
+
+        # Process the workload in the selected container
+        if not selected_container.process(workload):
+            # If the container is overloaded, request a new one and retry
+            self.request_new_container()
+            choice(self.containers).process(workload)
+
+    def select_container(self, workload):
+        # Implementing a strategy that merges Hermod and Cypress approaches
+        # Step 1: Prefer containers with the workload already present
+        # Step 2: If not found, use containers with least load considering input size
+
+        for container in self.containers:
+            if workload.job_name in container.get_job_names():
+                return container
+
+        # Calculate load considering the input size (Cypress approach)
+        # This is a placeholder for the actual logic, which should consider the workload's input size
+        for container in self.containers:
+            self.container_load[container.get_id()] = container.get_num_active_workloads()
+
+        # Select the container with the least load
+        least_loaded_container = min(self.container_load, key=self.container_load.get)
+        return next(container for container in self.containers if container.get_id() == least_loaded_container)
